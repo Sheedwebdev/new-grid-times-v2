63c72b641a16a3cd259425733193bc3acbd85c35
Rashid Enahora <rashidenahora@Rashids-MacBook-Pro.local>
2026-02-05 18:30:00 -0500

    Completed tablet and desktop heading section

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -29,16 +29,34 @@ const Header = () => {
         </Row>
       </SuperHeader>
       <MainHeader>
+        <DesktopActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </DesktopActionGroup>
         <Logo />
+        <SubscribeWrapper>
+          <Button>Subscribe</Button>
+          <SubLink href='/'>Already a subscriber?</SubLink>
+        </SubscribeWrapper>
       </MainHeader>
     </header>
   );
 };
 
+
+
 const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -59,12 +77,57 @@ const ActionGroup = styled.div`
   }
 `;
 
+const DesktopActionGroup = styled(ActionGroup)`
+  display: none;
+
+  @media ${QUERIES.laptopAndUp} {
+    display: flex;
+  }
+`;
+
+const SubscribeWrapper = styled.div`
+  display: none;
+  
+  @media ${QUERIES.laptopAndUp} {
+    display: revert;
+    justify-self: end;
+    position: relative;
+  }
+`;
+
+const SubLink = styled.a`
+  text-decoration: underline;
+  font-style: italic;
+  margin-top: 8px;
+  width: 100%;
+  text-align: center;
+  color: var(--color-gray-900);
+  font-size: 0.875rem;
+  position: absolute;
+`;
+
 const MainHeader = styled(MaxWidthWrapper)`
   display: flex;
   align-items: center;
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+  
+  @media ${QUERIES.tabletAndUp} {
+    margin-top: 48px;
+    margin-bottom: 72px;
+  }
+
+  @media ${QUERIES.laptopAndUp} {
+    display: grid;
+    align-items: center;
+    justify-content: revert;
+    grid-template-columns: 1fr auto 1fr;
+    justify-items: start;
+    margin-top: 16px;
+    margin-bottom: 72px;
+  }
 `;
+
 
 export default Header;
